a8f837bc2ad00a52c7e26c8a74222f6c19d2cccf
dudewheresmycode <brian@ndmweb.com>
2024-01-06 17:46:19 -0700

    powershell fixes

--- powershell/sendkey.ps1
+++ powershell/sendkey.ps1
@@ -1,12 +1,8 @@
 param([string]$window, [string]$key)
 
-. $PSScriptRoot\helpers\Wait-ForWindow.ps1
-
-$wshell = New-Object -ComObject wscript.shell;
+$wshell = New-Object -ComObject wscript.shell
 Write-Output "Activating window: $($window)"
 $wshell.AppActivate($window)
-# Sleep 1
-Wait-ForWindow -ProcessName $window -WindowTitle $window
 
 Write-Output "Sending key: $($key)"
 $wshell.SendKeys($key)
